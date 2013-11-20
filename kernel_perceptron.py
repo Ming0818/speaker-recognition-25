12e773243d8bcfcfbd515a31883b5633313c126b
thomas <thomas.sibutpinote@ens-lyon.fr>
2013-11-20 17:08:59 +0100

    on n'utilise plus b à part des autres paramètres

--- kernel_perceptron.py
+++ kernel_perceptron.py
@@ -6,22 +6,21 @@ import random
 import time
 import plot
 
-def ker_perceptron(x, y, k):
-    stop = 10000000
-    n = len(x)
+def ker_perceptron(oldX, y, k):
+    stop = 100000
+    n = len(oldX)
+    x = np.array([np.append(oldX[i],1) for i in xrange(n)])
     w = np.zeros(shape=(n,))
     w[0] = y[0]
-    b = y[0]
-    valeurs = np.array([y[i] * (w[0]*k(x[0], x[i]) + b) for i in xrange(n)])
+    valeurs = np.array([y[i] * w[0] * k(x[0], x[i]) for i in xrange(n)])
     negatifs = [i for i in xrange(n) if valeurs[i] < 0]
     while(negatifs and (stop > 0)):
         j = negatifs[0]
-        w[j] = w[j] + y[j]
-        b = b + y[j]
-        valeurs = np.add(valeurs, np.array([y[i]* y[j]*(k(x[j], x[i]) + 1) for i in xrange(n)]))
+        w[j] = w[j] + (y[j])
+        deltaValeurs = np.array([y[i]* (y[j])*(k(x[j], x[i])) for i in xrange(n)])
+        valeurs = np.add(valeurs, deltaValeurs)
         negatifs = [i for i in xrange(n) if valeurs[i] < 0]
         stop -= 1
-        plot.plot(x, y, w, b)
-    return w, b
+    return w
 
 
