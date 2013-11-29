969d7b3330c249078810e29105b3bd7fe2a48e8a
aplet <antoineplet@sfr.fr>
2013-11-29 18:18:01 +0100

    Modification de cepstraux.m : caluclu de 39 coefficients au lieu des 12 avant.

--- cepstraux.m
+++ cepstraux.m
@@ -1,6 +1,6 @@
 function cepstraux(wavfile, matfile)
   s=wavread(wavfile);
-  c=melcepst(s);
-  save(matfile,'c','-mat-binary')  
+  c=melcepst(s, 11025, 'e0dD');
+  save(matfile,'c', '-mat-binary');  
   exit
 end
