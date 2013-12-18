f6a0d645bc67e00e2cafa277dc5536ec5b3537a5
aplet <antoineplet@sfr.fr>
2013-12-18 18:53:26 +0100

    Il faut des guillemets doubles en octave, c'est mieux

--- cepstraux.m
+++ cepstraux.m
@@ -2,6 +2,6 @@ function cepstraux(wavfile, matfile)
   s=wavread(wavfile);
   c=melcepst(s, 11025, 'e0dD');
   [mu, sig, pi] = gaussmix(c, [], [], 4);
-  save(matfile, 'mu', 'sig', 'pi', '-mat-binary');  
+  save(matfile, "mu", "sig", "pi", "-mat-binary");  
   exit
 end
