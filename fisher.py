dddce36f49967ad0d2376a03c1ff985160c9fd58
thomas <thomas.sibutpinote@ens-lyon.fr>
2013-12-20 15:28:06 +0100

    dans le calcul de Fisher, on a besoin de pi_i et pi_j

--- fisher.py
+++ fisher.py
@@ -6,15 +6,15 @@ import octaveIO as oio
 def nim(m,pi,T):
     res = T*pi[m] #hypothèse à la lecture de l'article + http://www.ee.ic.ac.uk/hp/staff/dmb/voicebox/doc/voicebox/gaussmix.html
 
-def K(x,i,j,mu_i,mu_j,sig0,pi,mu0):
+def K(x,i,j,mu_i,mu_j,sig0,pi_i,pi_j,mu0):
     T = len(x)
     (M,F) = np.shape(mu) # M: nombre de Gaussiennes. F: nombre de features
     res = 0
     ni = np.zeros(M)
     nj = np.zeros(M)
     for m in xrange(M):
-        ni[m]=nim(m,pi,T)
-        nj[m]=nim(m,pi,T)
+        ni[m]=nim(m,pi_i,T)
+        nj[m]=nim(m,pi_j,T)
         ti = np.array([(mu_i[m,k] - mu0[k])/(sig0[k]^2) for k in xrange(F)])
         tj = np.array([(mu_j[m,k] - mu0[k])/(sig0[k]^2) for k in xrange(F)])
         res += ni[m]*nj[m]*np.dot(ti,tj)
