64e46e6c02adf54fee2fe8c6d8902a2b61c82f95
aplet <antoineplet@sfr.fr>
2013-12-20 17:43:40 +0100

    Tentative (pas encore concrétisée) de précalculer tout avant

--- cepstraux.m
+++ cepstraux.m
@@ -1,7 +1,7 @@
 function cepstraux(wavfile, matfile)
   s=wavread(wavfile);
   c=melcepst(s, 11025, 'e0dD');
-  [mu, sig, pi] = gaussmix(c, [], [], 4);
-  save(matfile, "c", "mu", "sig", "pi", "-mat-binary");  
+  #c=melcepst(s, 11025);
+  save(matfile, "c", "-mat-binary");  
   exit
 end
--- main.py
+++ main.py
@@ -4,36 +4,66 @@ import os,numpy as np,octaveIO as oio,string,subprocess
 
 
 
-def createDataFiles():
+def createDataFiles(nbc):
     if not os.path.exists('data'):
         os.makedirs('data')
         print "Please add some data, I don't work for free"
     else:
-        res = []
+        res = [] #contiendra l'ensemble des mfccs
+        muT=[]
+        piT=[]
+        dic={}
+        num=0
         for root, dirs, files in os.walk('data'):
-            print root,dirs,files
+            #print root,dirs,files
             for file in files:
                 if file.endswith(".wav"):
                     print "treating file "+file
                     name=os.path.splitext(file)[0]
                     fileName = os.path.join(root, name)
-                    wavName = fileName+'.wav'
-                    matName = fileName+'.mat'
-                    #print string.join(['octave','--eval','cepstraux('+'\''+wavName+'\',\''+matName+'\')'])
-                    subprocess.call(['octave','--eval','cepstraux('+'\''+wavName+'\',\''+matName+'\')'])
-                    triplet=oio.retrieve(matName,['c','mu','sig','pi'])
-                    res.append(triplet)
-        return res    
+                    wavFile = fileName+'.wav'
+                    mfccFile = fileName+'mfcc.mat' #contient 'c'
+                    gmmFile = fileName+'gmm.mat' #contient 'mu', 'sig' et 'pi'
+                    #print string.join(['octave','--eval','cepstraux('+'\''+wavFile+'\',\''+mfccFile+'\')'])
+                    subprocess.call(['octave', '--silent', '--eval','cepstraux('+'\''+wavFile+'\',\''+mfccFile+'\')'])
+                    c=oio.retrieve(mfccFile,['c'])[0]
+                    l=np.size(c, 0) #nombre total de frames
+                    nbv=l/nbc
+                    #print nbc, l, nbv
+                    dic[name]=[]
+                    for j in xrange(nbv):
+                        binf=j*nbc
+                        bsup=(j+1)*nbc
+                        #print 'binf, bsup :', binf, bsup
+                        newvf=c[binf:bsup] #vecteur de frames à ajouter
+                        #print np.shape(newvf)
+                        #print newvf
+                        res.append(newvf) #ajout dans l'ensemble complet
+
+                        #Calcul des gmm
+                        subprocess.call(['octave','--silent', '--eval','gmm(\''+mfccFile+'\', \''+gmmFile+'\')'])
+                        mu, pi = oio.retrieve(gmmFile, ['mu', 'pi'])
+                        muT.append(mu)
+                        piT.append(pi)
+                        dic[name].append(num)
+                        num+=1
+        return res, muT, piT, dic    
 
 def gmms(data):
-    c0 = np.concatenate([c for (c,mu,sig,pi) in data])
-    mfccFile = 'data/grossmMat.mat'
-    matName = 'data/grossMatOut.mat'
+    #print data
+    #print [np.shape(x) for x in data]
+    c0 = np.concatenate(data)
+    mfccFile = 'data/mfccs.mat'
+    gmmFile = 'data/gmms.mat'
     oio.write(mfccFile,c0,'c')
-    subprocess.call(['octave','--eval','gmm(\''+mfccFile+'\', \''+matName+'\')'])
-    triplet0=oio.retrieve(matName,['mu','sig','pi'])
+    #print string.join(['octave','--eval','gmm(\''+mfccFile+'\', \''+gmmFile+'\')'])
+    subprocess.call(['octave','--silent', '--eval','gmm(\''+mfccFile+'\', \''+gmmFile+'\')'])
+    mu0, sig0=oio.retrieve(gmmFile,['mu','sig'])
+    return mu0, sig0
 
-res = createDataFiles()
-print res
+res, mu, pi, dic = createDataFiles(100)
+#print res
+print dic
+print 'GMM sur l\'ensemble des points\n'
 
 gmms(res)
