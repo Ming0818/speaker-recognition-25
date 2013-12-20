ce79faaf2e7a84bd3b464c3ec83fc05bda5400bc
thomas <thomas.sibutpinote@ens-lyon.fr>
2013-12-20 16:17:51 +0100

    "build_labels" construit les labels à partir du nom du speaker à identifier "train" prend un nom et fait tourner le kernel perceptron à partir du nom et d'un dictionnaire

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # traitement global des fichiers wav
 import os,numpy as np,octaveIO as oio,string,subprocess
-
+import fisher
 
 
 def createDataFiles():
@@ -33,7 +33,26 @@ def gmms(data):
     subprocess.call(['octave','--eval','gmm(\''+mfccFile+'\', \''+matName+'\')'])
     triplet0=oio.retrieve(matName,['mu','sig','pi'])
 
-res = createDataFiles()
-print res
+# res = createDataFiles()
+# print res
 
-gmms(res)
+# gmms(res)
+
+
+def build_labels(name,dic,mu,pi):
+    size = sum(len(val) for val in dic.itervalues())
+    res = np.zeros(shape=(size,))
+    for key in dic:
+        if key==name:
+            val=1
+        else:
+            val=-1
+        for i in dick[key]:
+            res[i]=val
+
+def train(name,mu0,sig0,mu,pi):
+    y = build_labels(name,dic,mu,pi)
+    x=range(len(y))
+    k = lambda i,j : fisher.K(x,i,j,mu[i],mu[j],sig0,pi[i],pi[j],mu0)
+    w = ker_perceptron(x,y,k)
+    return w
