6b39fef0d0d1f847979dac1995b8637f47f1411e
aplet <antoineplet@sfr.fr>
2014-01-04 13:56:48 +0100

    Plus de coefficients DCT dans cepstraux

--- cepstraux.m
+++ cepstraux.m
@@ -1,7 +1,6 @@
 function cepstraux(wavfile, matfile)
   [s,fs]=wavread(wavfile);
-  c=melcepst(s, fs, 'e0dD');
-  #c=melcepst(s, fs);
+c=melcepst(s, fs, 'e0dD', 16);
   save(matfile, "c", "-mat-binary");  
   exit
 end
